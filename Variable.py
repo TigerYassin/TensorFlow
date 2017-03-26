6778dd036e735ac27a2a43def543473f9be07dbb
Yassin <yassin@incorporated.org>
2017-03-26 14:08:01 -0400

    finished covering variables in tensorflow

--- Variable.py
+++ Variable.py
@@ -1,19 +1,25 @@
-# import tensorflow as tf
-#
-# state = tf.Variable(0)
-#
-# one = tf.constant(1)
-# new_value = tf.add(state, one)
-# update = tf.assign(state, new_value)
-#
-# init_op = tf.global_variables_initializer()
-#
-# with tf.Session() as session:
-#     session.run(init_op)
-#     print(session.run(state))
-#     for _ in range(3):
-#         session.run(update)
-#         print(session.run(state))
+"""
+working with variables in tensorflow
+i broke it down into sections so that you can comment out sections at a time
+"""
+
+
+import tensorflow as tf
+
+state = tf.Variable(0)
+
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+init_op = tf.global_variables_initializer()
+
+with tf.Session() as session:
+    session.run(init_op)
+    print(session.run(state))
+    for _ in range(3):
+        session.run(update)
+        print(session.run(state))
 
 
 
@@ -28,6 +34,7 @@ theVar = tf.Variable(10)
 theConstant = tf.constant(2)
 
 add = tf.add(theVar, theConstant)
+update = tf.assign(theVar, add)         #The Constant cannot be changed, only a variable is able to be reassigned. Must be a tensorflow variable
 
 init_Var = tf.global_variables_initializer()        #must do this to initialize all the variables, must be done after the variables
 
@@ -37,5 +44,45 @@ with tf.Session() as sess:
     print(sess.run(theVar))
     for x in range(5):
         print(sess.run(add))
+        print(sess.run(update))
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+import tensorflow as tf
+
+theVar = tf.Variable(4)
+theConstant = tf.constant(10)
+
+added = tf.add(theVar, theConstant)
+new_val = tf.assign(theVar, added)
+
+init_var = tf.global_variables_initializer()
+
+with tf.Session() as sess:            #must instantiate a session
+    sess.run(init_var)
+    print(sess.run(added))
+    print(sess.run(theConstant))    #in order to show readable data, u must run the object in the session
+    print(theVar)               # this obeject is not run in the session and it looks confusing
+    for x in range(8):
+        print(sess.run(new_val))
+
+with tf.Session() as what:      #another session, each session stores its own shit
+    what.run(init_var)
+    print(what.run(new_val))    # doesn't continue to the from the values the were assigned in the previous session, but instead start everything over
